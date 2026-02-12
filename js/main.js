e5aa1b9c5c64d95c576dc40fc3cb70a098053631
michaelgorotiza2007-hub <michael.gorotiza2007@gmail.com>
2026-02-12 15:49:01 -0500

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-window.openModal = () => document.getElementById('modal').classList.remove('hidden');
-const form = document.getElementById('preForm');
-if(form) {
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        localStorage.setItem('preData', JSON.stringify({ cargo: document.getElementById('cargo').value }));
-        window.location.href = 'register.html';
-    });
-}
